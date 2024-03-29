1482259efed742791ff028ef22717c43837fc33f
Kristian MayesKristians-MBP:~ kristianmayes$ git config --global user.name Kristian <93098425+Kmayes5@users.noreply.github.com>
2024-03-29 00:52:13 -0400

    SDKC1

---
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck1_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello. Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
+
+            var input = Console.ReadLine();
+            var calculator = new Calculator();
+
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("Enter 2 integers to add");
+                    int addNumber1 = 0;
+                    int addNumber2 = 0;
+
+                    try
+                    {
+                        UserInputHelper.PromptForTwoNumbers(ref addNumber1, ref addNumber2);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("One or more of the numbers is not an int");
+                    }
+
+                        Console.Write($"{addNumber1} + {addNumber2} = ");
+                        Console.Write(calculator.Add(addNumber1, addNumber2));
+
+                        Console.ReadLine();
+                    break;
+
+                case "2":
+                    Console.WriteLine("Enter 2 integers to subtract");
+                    int subtractNumber1 = 0;
+                    int subtractNumber2 = 0;
+
+                    try
+                    {
+                        UserInputHelper.PromptForTwoNumbers(ref subtractNumber1, ref subtractNumber2);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("One or more of the numbers is not an int");
+                    }
+                    break;
+
+                case "3":
+                    Console.WriteLine("Enter 2 intrgers to muiltply");
+                    int multiplyNumber1 = 0;
+                    int multiplyNumber2 = 0;
+
+                    try
+                    {
+                        UserInputHelper.PromptForTwoNumbers(ref multiplyNumber1, ref multiplyNumber2);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("One or more of the numbers is not an int");
+                    }
+                    break;
+
+                case "4":
+                    Console.WriteLine("Enter 2 integers to divide");
+                    int divideNumber1 = 0;
+                    int divideNumber2 = 0;
+
+                    try
+                    {
+                        UserInputHelper.PromptForTwoNumbers(ref divideNumber1, ref divideNumber2);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("One or more of the numbers is not an int");
+                    }
+                    break;
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown input");
+                    break;
+            }
+        }
+    }
+}
---
+++ KnowledgeCheck1_Calculator/UserInputHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace KnowledgeCheck1_Calculator
+{
+	internal static class UserInputHelper
+	{
+		internal static void PromptForTwoNumbers(ref int num1, ref int num2) {
+
+           var num1string = Console.ReadLine();
+           var num2string = Console.ReadLine();
+			
+			if(int.TryParse(num1string, out num2)&&int.TryParse(num2string, out num1))
+			{
+				return;
+			}
+			else {
+				throw new Exception("input not a number");
+			}
+        }
+
+			
+	}
+}
+
